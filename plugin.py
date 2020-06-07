6c16257f1410f77246a7565c3112c6d5b725f387
Arnold Kamminga <akamming@users.noreply.github.com>
2020-06-07 08:45:14 +0200

    fixed some bugs

--- plugin.py
+++ plugin.py
@@ -23,7 +23,7 @@ debug=True
     
 def Debug(text):
     if (debug):
-        Domoticz.Log("DEBUG: "+str(text))
+        Domoticz.Log("Debug: "+str(text))
 
 
 def UpdateCounterSensor(SensorName,UnitID,Value):
@@ -33,11 +33,34 @@ def UpdateCounterSensor(SensorName,UnitID,Value):
             Domoticz.Device(Name=SensorName, Unit=UnitID, TypeName="Counter").Create()
         Debug ("Updating "+SensorName+"("+str(UnitID)+") with value "+str(Value))
         Devices[UnitID].Update(nValue=int(Value), sValue=str(Value))
+        Domoticz.Log("Counter ("+str(SensorName)+")")
+
+def UpdateCustomSensor(SensorName,UnitID,Value):
+       #Creating devices in case they aren't there...
+        if not (UnitID in Devices):
+            Debug("Creating device "+SensorName)
+            Domoticz.Device(Name=SensorName, Unit=UnitID, TypeName="Custom").Create()
+        Debug ("Updating "+SensorName+"("+str(UnitID)+") with value "+str(Value))
+        #Devices[UnitID].Update(nValue=float(Value), sValue=str(Value))
+        Devices[UnitID].Update(nValue=0, sValue=str(Value))
+        Domoticz.Log("Counter ("+str(SensorName)+")")
+
+def UpdatePercentageSensor(SensorName,UnitID,Value):
+       #Creating devices in case they aren't there...
+        if not (UnitID in Devices):
+            Debug("Creating device "+SensorName)
+            Domoticz.Device(Name=SensorName, Unit=UnitID, TypeName="Percentage").Create()
+        Debug ("Updating "+SensorName+"("+str(UnitID)+") with value "+str(Value))
+        Devices[UnitID].Update(nValue=int(Value), sValue=str(Value))
+        #Devices[UnitID].Update(nValue=0, sValue=str(Value))
+        Domoticz.Log("Percentage ("+str(SensorName)+")")
 
 class BasePlugin:
     enabled = False
 
     def UpdateSensors(self):
+        global timestamp
+
         # Make a get request to get the  data
         response = requests.get(url)
 
@@ -45,15 +68,32 @@ class BasePlugin:
         if (response.status_code==200):
             Debug("Retrieve data:")
             data=response.json()
+
+            # do some debugging
             for key,value in data.items():
                 if type(value)==dict:
                     Debug("Key="+key+" = "+str(data[key]["value"])+"(dict)")
                 else:
                     Debug("Key="+key+" = "+str(value)+"(str)")
+
+            #Update the sensors
+
+            UpdateCustomSensor("Intensive care-opnames per dag",1,data["intake_intensivecare_ma"]["value"])
+            UpdateCustomSensor("Ziekenhuis opnames per dag",2,data["intake_hospital_ma"]["value"])
+            UpdateCustomSensor("Positief getest mensen per dag (per 100.000 inwoners)",3,data["infected_people_delta_normalized"]["value"])
+            UpdateCustomSensor("Aantal bestmettelijke mensen (per 100.000 inwoners)",4,data["infectious_people_count_normalized"]["value"])
+            UpdateCustomSensor("Totaal aantal besmettelijke mensen",5,data["infectious_people_count"]["value"])
+            UpdatePercentageSensor("Reproductiegetal (percentage)",6,float(data["reproduction_index"]["value"]*100))
+            UpdateCustomSensor("Positief geteste verpleeghuisbewoners per dag",7,data["infected_people_nursery_count_daily"]["value"])
+            UpdateCustomSensor("Overleden verpleeghuisbewoners per dag",8,data["deceased_people_nursery_count_daily"]["value"])
+
+
+            #Update the timestamp to prevent too many requests to the json call
+            timestamp=datetime.datetime.now()
+
         else:
             Domoticz.Log("Error getting coronadashboard date: "+str(response.status_code))
 
-        UpdateCounterSensor("Intensive care-opnames per dag",1,data["intake_intensivecare_ma"]["value"])
 
 
 
@@ -62,32 +102,39 @@ class BasePlugin:
         return
 
     def onStart(self):
-        Domoticz.Log("onStart called")
+        Debug("onStart called")
         timestamp=datetime.datetime.now()
         self.UpdateSensors()
 
 
     def onStop(self):
-        Domoticz.Log("onStop called")
+        Debug("onStop called")
 
     def onConnect(self, Connection, Status, Description):
-        Domoticz.Log("onConnect called")
+        Debug("onConnect called")
 
     def onMessage(self, Connection, Data):
-        Domoticz.Log("onMessage called")
+        Debug("onMessage called")
 
     def onCommand(self, Unit, Command, Level, Hue):
-        Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+        Debug("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
 
     def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
-        Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
+        Debug("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
 
     def onDisconnect(self, Connection):
-        Domoticz.Log("onDisconnect called")
+        Debug("onDisconnect called")
 
     def onHeartbeat(self):
-        Domoticz.Log("onHeartbeat called")
-        self.UpdateSensors()
+        Debug("onHeartbeat called")
+
+        ElapsedTime=datetime.datetime.now()-timestamp
+        if (ElapsedTime.total_seconds()>interval):
+            #enough time passed, let's update the sensors
+            self.UpdateSensors()
+        else:
+            #not enough time passed
+            Debug("Elapsed time = "+str(ElapsedTime))
 
 global _plugin
 _plugin = BasePlugin()
