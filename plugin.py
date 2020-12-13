861b2cb524245b18494f102d3f9bae60345ad19b
root <root@pi>
2020-12-13 15:58:56 +0100

    Fixed infectious_people_total and infectious_people_normalized

--- plugin.py
+++ plugin.py
@@ -97,11 +97,18 @@ class BasePlugin:
                 Debug("LastUpdate("+str(LastUpdate)+") > LastDashboardUpdate("+str(LastDashboardUpdate)+"), updating sensors...")
                 Domoticz.Log("New data available at dashboard, updating sensors...")
 
+                #Calculate last know value of infectious people normalized from JSON
+                infectious_people_count_normalized=0
+                for i in data["infectious_people_count_normalized"]["values"]:
+                    if i["infectious_avg_normalized"] != None:
+                        infectious_people_count_normalized=i["infectious_avg_normalized"]
+
                 #Update the sensors
                 UpdateCustomSensor("Intensive care-opnames per dag",1,data["intake_intensivecare_ma"]["last_value"]["moving_average_ic"])
                 UpdateCustomSensor("Ziekenhuis opnames per dag",2,data["intake_hospital_ma"]["last_value"]["moving_average_hospital"])
                 UpdateCustomSensor("Positief getest mensen per dag (per 100.000 inwoners)",3,data["infected_people_delta_normalized"]["last_value"]["infected_daily_increase"])
-                UpdateCustomSensor("Aantal bestmettelijke mensen (per 100.000 inwoners)",4,data["infectious_people_count_normalized"]["last_value"]["infectious_avg_normalized"])
+                #UpdateCustomSensor("Aantal bestmettelijke mensen (per 100.000 inwoners)",4,data["infectious_people_count_normalized"]["last_value"]["infectious_avg_normalized"])
+                UpdateCustomSensor("Aantal bestmettelijke mensen (per 100.000 inwoners)",4,infectious_people_count_normalized)
                 UpdateCustomSensor("Totaal aantal besmettelijke mensen",5,data["infectious_people_last_known_average"]["last_value"]["infectious_avg"])
                 if (data["reproduction_index_last_known_average"]["last_value"]["reproduction_index_avg"]==None):
                     Debug("reproduction index = none")
